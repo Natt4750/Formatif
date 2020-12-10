c47b0911e32c9f4e1ec76f2aa32b562d883f0433
nasse <nasse@10.5.2.75>
2020-12-10 11:24:20 -0500

    Premier Commit

---
+++ Formatif/src/Formatif_Nasser.java
@@ -0,0 +1,14 @@
+/*
+* Author : nasser
+*Date : Dec. 10, 2020
+*/
+public class Formatif_Nasser
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+	}
